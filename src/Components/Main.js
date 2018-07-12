6bdf747b872ef863a9ab4f756a8bdf8d9e505302
mahmudaZaman <mahmudazaman0509@gmail.com>
2018-07-12 17:45:07 +0600

    initial commit

---
+++ src/Components/Main.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Main extends Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="box">O</div>
+        <div className="box">O</div>
+        <div className="box">O</div>
+        <div className="box">O</div>
+        <div className="box">O</div>
+        <div className="box">O</div>
+      </div>
+    );
+  }
+}
+
+export default Main;
